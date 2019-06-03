9f2b54c69b26be36f463bd6565963b1e8fcf2439
shuntaowang <34738044+shuntaowang@users.noreply.github.com>
2019-06-03 18:45:58 +0800

    Add files via upload
    添加见渐隐渐显  和添加对象移动中间连线动态变化

---
+++ UICommonScritpts/DynamicConnectLine_1.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 对两个对象之间进行动态连线
+/// </summary>
+public class DynamicConnectLine_1 : MonoBehaviour
+{
+    /// <summary>
+    /// 左边的对象
+    /// </summary>
+    public Transform LeftObj;
+    /// <summary>
+    /// 右边的对象
+    /// </summary>
+    public Transform RightObj;
+
+    /// <summary>
+    /// 连线
+    /// </summary>
+    public Transform MiddleLineObj;
+    UISprite MiddleLineObjSprite;
+    // Use this for initialization
+    void Start()
+    {
+        MiddleLineObjSprite = MiddleLineObj.GetComponent<UISprite>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawLine();
+    }
+
+    void DrawLine()
+    {
+        float x = (RightObj.localPosition.x - LeftObj.localPosition.x) / 2 + LeftObj.localPosition.x;
+        float y = (RightObj.localPosition.y - LeftObj.localPosition.y) / 2 + LeftObj.localPosition.y;
+        MiddleLineObj.localPosition = new Vector3(x, y, MiddleLineObj.localPosition.z);
+        MiddleLineObj.localRotation = Quaternion.Euler(0, 0, GetAngle(LeftObj.localPosition, RightObj.localPosition));
+        if (MiddleLineObjSprite)
+        {
+            float distance = Vector3.Distance(LeftObj.localPosition, RightObj.localPosition);
+            MiddleLineObjSprite.width = (int)distance;
+        }
+    }
+    
+    /// <summary>
+    /// 获取两个对象之间的角度值
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    private float GetAngle(Vector3 a, Vector3 b)
+    {
+        if (a.x - b.x != 0 && a.y - b.y == 0)
+        {
+            return 0;
+        }
+        if (a.x - b.x == 0 && a.y - b.y != 0)
+        {
+            return 90;
+        }
+        float angle = Mathf.Atan((a.y - b.y) / (a.x - b.x)) * Mathf.Rad2Deg;
+        return angle;
+    }
+}
---
+++ UICommonScritpts/FadeOutGraduallyShow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 渐隐渐显  父节点下的所有字节点进行不断的渐隐渐显的效果显示
+/// </summary>
+public class FadeOutGraduallyShow : MonoBehaviour
+{
+    List<Transform> transforms = new List<Transform>();
+    public float times; //实现次数
+    public float delayTime;//延迟执行时间
+    public float internalTime;//间隔时间
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform item in transform)
+        {
+            transforms.Add(item);
+
+        }
+        FadeOutOrFadeOutGraduallyShow();
+        InvokeRepeating("FadeOutOrFadeOutGraduallyShow", 2, (delayTime+internalTime)*2+0.15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    int excuteTimes = 0;
+    void FadeOutOrFadeOutGraduallyShow()
+    {
+        if (excuteTimes >= times)
+            CancelInvoke("FadeOutOrFadeOutGraduallyShow");
+        excuteTimes++;
+        foreach (Transform item in transforms)
+        {
+            UISprite uISprite = item.GetComponent<UISprite>();
+            if (uISprite == null)
+                uISprite = item.gameObject.AddComponent<UISprite>();
+
+            TweenAlpha tweenAlpha = uISprite.GetComponent<TweenAlpha>();
+            if (tweenAlpha == null)
+                tweenAlpha = uISprite.gameObject.AddComponent<TweenAlpha>();
+            tweenAlpha.delay = delayTime;
+            tweenAlpha.duration = internalTime;
+            tweenAlpha.from = 1;
+            tweenAlpha.to = 0;
+            tweenAlpha.PlayForward();
+            tweenAlpha.AddOnFinished(() => { tweenAlpha.PlayReverse(); });
+        }
+    }
+}
